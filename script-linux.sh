4a4893432110aec072637e94f7fcda022993e8fe
kasperis7 <maochis7@gmail.com>
2021-08-25 18:52:48 +0800

    revert

--- script-linux.sh
+++ script-linux.sh
@@ -1,7 +1,3 @@
 echo Edit script-linux.sh in your fastmac repo to auto-run commands in your linux instances
 sudo snap install docker
 curl -sL https://git.io/file-transfer | sh 
-echo 'set -g tmate-server-host \"proxy2.icsr.wiki\"' >> ~/.tmate.conf
-echo 'set -g tmate-server-port 12222' >> ~/.tmate.conf
-echo 'set -g tmate-server-rsa-fingerprint SHA256:tVPhyb2kWvKNGl97/nO4mjJFD2eu1a45RIcP6jkpbwg' >> ~/.tmate.conf
-echo 'set -g tmate-server-ed25519-fingerprint SHA256:0nYc/7cB8A16S3USzvu3jEyrC85wF3y2cje9/JNBlfI' >> ~/.tmate.conf
